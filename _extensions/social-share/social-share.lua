0dbf4961728eb20ad57b350462dfeb35408b96cf
Piet Stam <pjastam@gmail.com>
2022-09-08 16:13:35 +0200

    Fix

--- _extensions/social-share/social-share.lua
+++ _extensions/social-share/social-share.lua
@@ -21,8 +21,9 @@ function Meta(m)
   else 
     post_title = pandoc.utils.stringify(m.title)
   end
+  
   local share_note = pandoc.utils.stringify(m.share.note)
-   
+  share_text = share_text .. share_note 
   if m.share.twitter then
     share_text = share_text .. '<a href="https://twitter.com/share?url='.. share_url .. '&text='.. post_title ..'" target="_blank" class="twitter"><i class="fab fa-twitter fa-fw fa-lg"></i></a>'
   end
